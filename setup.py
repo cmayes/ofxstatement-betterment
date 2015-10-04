56ac15f76b9b36bfb3e71f3b923bb4579369ad79
Chris Mayes <cmayes@cmayes.com>
2015-10-04 09:13:38 -0500

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 ]
 
 setup(name='ofxstatement-betterment',
-      version='0.2.1',
+      version='0.2.2',
       author="Chris Mayes",
       author_email="cmayes@cmay.es",
       url="https://github.com/cmayes/ofxstatement-betterment",

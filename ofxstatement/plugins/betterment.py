ea13fb30a13e7d7109a6b54118b39637549cc88e
Chris Mayes <cmayes@cmayes.com>
2015-10-18 09:21:22 -0500

    Bump version: 0.2.4 → 0.2.5

--- ofxstatement/plugins/betterment.py
+++ ofxstatement/plugins/betterment.py
@@ -2,7 +2,7 @@ import hashlib
 
 __author__ = 'Chris Mayes'
 __email__ = 'cmayes@cmay.es'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 import csv
 from ofxstatement.plugin import Plugin
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 ]
 
 setup(name='ofxstatement-betterment',
-      version='0.2.4',
+      version='0.2.5',
       author="Chris Mayes",
       author_email="cmayes@cmay.es",
       url="https://github.com/cmayes/ofxstatement-betterment",
